41e3151cb78eeba4538d415a7e0915fac75b62c7
Phillip Look <phillip.look@brainbits.net>
2024-02-24 02:14:00 +0100

    chore: Activate more rector rules

--- rector.php
+++ rector.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
+use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
+use Rector\PHPUnit\Rector\Class_\PreferPHPUnitSelfCallRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -14,8 +19,21 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/rector.php',
     ]);
 
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon.dist');
+
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_82,
+
+        PHPUnitSetList::PHPUNIT_40,
+        PHPUnitSetList::PHPUNIT_50,
+        PHPUnitSetList::PHPUNIT_60,
+        PHPUnitSetList::PHPUNIT_70,
+        PHPUnitSetList::PHPUNIT_80,
+        PHPUnitSetList::PHPUNIT_90,
+        PHPUnitSetList::PHPUNIT_100,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
+
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE,
         SetList::DEAD_CODE,
@@ -27,7 +45,15 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::INSTANCEOF,
     ]);
 
+    $rectorConfig->rules([
+        AddLiteralSeparatorToNumberRector::class,
+        DeclareStrictTypesRector::class,
+        PreferPHPUnitSelfCallRector::class,
+    ]);
+
     $rectorConfig->skip([
+        PreferPHPUnitThisCallRector::class,
+        // phpcs:ignore Squiz.Arrays.ArrayDeclaration.KeySpecified
         RenamePropertyToMatchTypeRector::class => [
             __DIR__ . '/tests',
         ],
